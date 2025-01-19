ade8b6b8922a7cd11a2cc9a7988456a2d17f25f9
Yadunund <yadunund@gmail.com>
2025-01-19 11:15:45 -0800

    Switch to model_dir getting and linting changes
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- ibpc_pose_estimator/src/ibpc_pose_estimator.hpp
+++ ibpc_pose_estimator/src/ibpc_pose_estimator.hpp
@@ -1,18 +1,19 @@
-// TODO: Copyright.
+// TODO(Yadunund): Copyright.
 
-#ifndef IPBC_POSE_ESTIMATOR__IBPC_POSE_ESTIMATOR_HPP_
-#define IPBC_POSE_ESTIMATOR__IBPC_POSE_ESTIMATOR_HPP_
+#ifndef IBPC_POSE_ESTIMATOR_HPP_
+#define IBPC_POSE_ESTIMATOR_HPP_
+
+#include <filesystem>
+#include <memory>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 
 #include "ibpc_interfaces/msg/pose_estimate.hpp"
 #include "ibpc_interfaces/srv/get_pose_estimates.hpp"
 
-#include <filesystem>
-#include <memory>
-#include <vector>
-
-namespace ibpc {
+namespace ibpc
+{
 
 //==================================================================================================
 class PoseEstimator : public rclcpp::Node
@@ -21,18 +22,22 @@ public:
   using GetPoseEstimates = ibpc_interfaces::srv::GetPoseEstimates;
   using PoseEstimate = ibpc_interfaces::msg::PoseEstimate;
 
-  PoseEstimator(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+  /// Constructor.
+  explicit PoseEstimator(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
 
-  bool load_model(const std::filesystem::path & model_path);
+  // Get the path to the directory in the local filesystem that contains various models
+  // and other files required for inference.
+  const std::filesystem::path & model_dir() const;
 
   // TODO(Yadunund): Replace request with concrete args that participants will be familiar wiht.
   std::vector<PoseEstimate> get_pose_estimates(
     std::shared_ptr<const GetPoseEstimates::Request> request);
 
 private:
+  std::filesystem::path model_dir_;
   std::shared_ptr<rclcpp::Service<GetPoseEstimates>> srv_;
 };
 
 }  // namespace ibpc
 
-#endif  // IPBC_POSE_ESTIMATOR__IBPC_POSE_ESTIMATOR_HPP_
+#endif  // IBPC_POSE_ESTIMATOR_HPP_
