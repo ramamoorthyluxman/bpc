cc8fd35827f0449c6a7016be7b10f696085fc36c
Yadunund <yadunund@gmail.com>
2025-01-19 13:01:39 -0800

    Provide camera model
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- ibpc_pose_estimator/src/ibpc_pose_estimator.hpp
+++ ibpc_pose_estimator/src/ibpc_pose_estimator.hpp
@@ -11,6 +11,7 @@
 #include "ibpc_interfaces/srv/get_pose_estimates.hpp"
 
 #include "cv_bridge/cv_bridge.hpp"
+#include "image_geometry/pinhole_camera_model.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 
@@ -24,22 +25,40 @@ public:
   using GetPoseEstimates = ibpc_interfaces::srv::GetPoseEstimates;
   using PoseEstimate = ibpc_interfaces::msg::PoseEstimate;
 
-  /// Constructor.
+  /// @brief Construct a PoseEstimator
+  /// @param options Options for the ROS 2 node.
   explicit PoseEstimator(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
 
-  /// Get the path to the directory in the local filesystem that contains various models
-  /// and other files required for inference.
+  /// @brief Get the path to the directory in the local filesystem that contains various models
+  ///   and other files required for inference.
+  /// @return Path to model directory which can be used to load any models.
   const std::filesystem::path & model_dir() const;
 
-  /// Function for participants to implement to return pose estimates for objects in a scene.
+  /// @brief Function for participants to implement to return pose estimates for objects in a scene.
+  /// @param object_ids A list of object IDs in the scene whose pose should be estimated.
+  /// @param rgb A cv::Mat containing the RGB image.
+  /// @param rgb_camera_model The camera model of the RGB sensor.
+  /// @param depth A cv::Mat containing the depth image.
+  /// @param depth_camera_model The camera model of the depth sensor.
+  /// @param polarized A cv::Mat containing the polarized image.
+  /// @param polarized_camera_model The camera model of the polarized sensor.
+  /// @return List of objects detected in the scene as a PoseEstimate message.
+  /// @note For details on cv::Mat see https://docs.opencv.org/4.6.0/d3/d63/classcv_1_1Mat.html
+  /// @note For details on image_geometry::PinholeCameraModel see
   std::vector<PoseEstimate> get_pose_estimates(
     const std::vector<uint64_t> & object_ids,
     const cv::Mat & rgb,
+    const image_geometry::PinholeCameraModel & rgb_camera_model,
     const cv::Mat & depth,
-    const cv::Mat & polarized);
+    const image_geometry::PinholeCameraModel & depth_camera_model,
+    const cv::Mat & polarized,
+    const image_geometry::PinholeCameraModel & polarized_camera_model);
 
 private:
   std::filesystem::path model_dir_;
+  image_geometry::PinholeCameraModel rgb_camera_model_;
+  image_geometry::PinholeCameraModel depth_camera_model_;
+  image_geometry::PinholeCameraModel polarized_camera_model_;
   std::shared_ptr<rclcpp::Service<GetPoseEstimates>> srv_;
 };
 
