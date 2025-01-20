01566981614e14912501e7ef63a4d575346b27ab
Yadunund <yadunund@gmail.com>
2025-01-19 17:44:05 -0800

    Parse test split and query results
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- ibpc_tester/setup.py
+++ ibpc_tester/setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'ibpc_tester'
+package_name = "ibpc_tester"
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Yadunund Vijay',
-    maintainer_email='yadunund@gmail.com',
-    description='TODO: Package description',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer="Yadunund Vijay",
+    maintainer_email="yadunund@gmail.com",
+    description="TODO: Package description",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'ibpc_tester = ibpc_tester.ibpc_tester:main',
+        "console_scripts": [
+            "ibpc_tester = ibpc_tester.ibpc_tester:main",
         ],
     },
 )
